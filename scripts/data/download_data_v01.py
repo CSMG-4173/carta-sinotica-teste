4c7d003ed73ac9d44df8ac50894fa159ded5eede
João Gabriel Martins Ribeiro <100958609+Joaogmr472@users.noreply.github.com>
2024-06-12 11:04:22 -0300

    Update download_data_v01.py

--- scripts/data/download_data_v01.py
+++ scripts/data/download_data_v01.py
@@ -30,6 +30,12 @@ def limpar_arquivos_antigos(diretorio, dias=7):
                 print(f'Removendo arquivo antigo: {file_path}')
                 os.remove(file_path)
 
+# Limpar arquivos antigos
+limpar_arquivos_antigos(f'{output_dir}GFS/')
+limpar_arquivos_antigos(f'{output_dir}METAR/')
+limpar_arquivos_antigos(f'{output_dir}GOES/')
+
+
 # Obter a data e hora atuais
 now = datetime.utcnow()
 
@@ -42,12 +48,15 @@ nearest_desired_hour = max(filter(lambda x: x <= now.hour, desired_hours))
 # Crie a hora arredondada para a URL
 rounded_time = now.replace(hour=nearest_desired_hour, minute=0, second=0, microsecond=0)
 
-
 # Extrair dia, mês, ano e hora
 dia = rounded_time.strftime('%d')
 mes = rounded_time.strftime('%m')
 ano = rounded_time.strftime('%Y')
 hora = rounded_time.strftime('%H')
+
+##########################################################################################################
+############################## METAR #####################################################################
+##########################################################################################################
 
 # Construa o URL do METAR com a hora arredondada
 metar_url = f"https://thredds.ucar.edu/thredds/fileServer/noaaport/text/metar/metar_{ano}{mes}{dia}_{hora}00.txt"
@@ -57,11 +66,14 @@ output_dir = "scripts/data/"
 
 # Nome do arquivo METAR com data e hora
 metar_file_name = f"metar_data_{ano}{mes}{dia}_{hora}00.txt"
-metar_file_path = os.path.join(output_dir, 'METAR/', metar_file_name)
+metar_file_path = os.path.join(output_dir, 'METAR', metar_file_name)
 
 # Faz o download do arquivo METAR
 urllib.request.urlretrieve(metar_url, metar_file_path)
 
+##########################################################################################################
+################################ GFS #####################################################################
+##########################################################################################################
 # Start the time counter
 start_time = t.time()
 
@@ -85,7 +97,7 @@ print(url)
 
 # Nome do arquivo a ser baixado
 file_name = f'gfs.t{ano}{dia}{hora}z.pgrb2.0p{resolution}.f000'
-file_path = os.path.join(output_dir, 'GFS/', file_name)
+file_path = os.path.join(output_dir, 'GFS', file_name)
 
 # Print do nome do arquivo
 print("File name: ", file_name)
@@ -109,6 +121,29 @@ print(f'Run: {hora}')
 # End the time counter
 print('\nTotal Processing Time:', round((t.time() - start_time), 2), 'seconds.')
 
-# Limpar arquivos antigos
-limpar_arquivos_antigos(output_dir)
+##########################################################################################################
+############################################ GOES ########################################################
+##########################################################################################################
+
+ftp_cptec = 'http://ftp.cptec.inpe.br/goes/'
+
+file_vis = f'{ftp_cptec}goes16/retangular/ch02/{ano}/{mes}/S10635334_{ano}{mes}{dia}{hora}00.nc'
+file_ir =  f'{ftp_cptec}goes16/retangular/ch13/{ano}/{mes}/S10635346_{ano}{mes}{dia}{hora}00.nc'
+file_wv =  f'{ftp_cptec}goes16/retangular/ch08/{ano}/{mes}/S10635340_{ano}{mes}{dia}{hora}00.nc'
+
+goes_output_dir = os.path.join(output_dir, 'GOES')
+
+def download_file(url, output_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(output_path, 'wb') as f:
+            f.write(response.content)
+        print(f"Arquivo baixado: {output_path}")
+    else:
+        print(f"Erro ao baixar {url}: Status {response.status_code}")
+
+# Baixar arquivos GOES
+download_file(file_vis, os.path.join(goes_output_dir, f'S10635334_VIS_{ano}{mes}{dia}{hora}00.nc'))
+download_file(file_ir, os.path.join(goes_output_dir, f'S10635346_IR_{ano}{mes}{dia}{hora}00.nc'))
+download_file(file_wv, os.path.join(goes_output_dir, f'S10635340_WV_{ano}{mes}{dia}{hora}00.nc'))
 
